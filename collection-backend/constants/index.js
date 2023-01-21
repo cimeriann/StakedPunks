d70bcc79d8d1bf5a29e0996a2724db72253c92dd
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-01-21 21:57:25 +0100

    fix error: invalid tokenURI

--- collection-backend/constants/index.js
+++ collection-backend/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract
-const WHITELIST_CONTRACT_ADDRESS = "0xEbb5615eED298e34c55B804705EB877736Dd4a08";
+const WHITELIST_CONTRACT_ADDRESS = "0xB898cfC882928a21ACbcC7Fd83cb42c9b3E72c3f";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://staked-punks.vercel.app/api/";
 const STAKEDPUNKS_ADDRESS = "0x5665476eD96C5153CFaeBA8E52661885E0656d0C";
