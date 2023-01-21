97e27036de296de10b580c1fbde53bfbeb0b92fc
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-01-21 21:36:42 +0100

    enable opensea verification

--- collection-backend/constants/index.js
+++ collection-backend/constants/index.js
@@ -1,7 +1,7 @@
 // Address of the Whitelist Contract
 const WHITELIST_CONTRACT_ADDRESS = "0xEbb5615eED298e34c55B804705EB877736Dd4a08";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://staked-punks.vercel.app/api/";
 const STAKEDPUNKS_ADDRESS = "0x5665476eD96C5153CFaeBA8E52661885E0656d0C";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
